4eb32d4487ed117e3c230294d84eac2dc48f7546
Datreus <jenielgr10@gmail.com>
2026-05-04 22:04:32 -0400

    Checked body and attractor update loops, applied newtons third law, added trails

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <deque>
 
 #define SCALE 2.0f
 #define ATTRACTOR_MASS 500.0f
 #define ATTRACTOR_RADIUS 10.0f
-#define G 100.0f // Gravitational constant in arbitrary units
+#define G 1000.0f // Gravitational constant in arbitrary units
 #define SOFTENING 50.0f // Softening factor to prevent singularity at zero distance
-
-
-
 
 //Calculate the magnitude and return a normalized vector in the same direction as the input vector.
 sf::Vector2f normalize(sf::Vector2f vector)
@@ -19,6 +17,13 @@ sf::Vector2f normalize(sf::Vector2f vector)
     double magnitude = std::sqrt(std::pow(vector.x, 2) + std::pow(vector.y, 2));
     if (magnitude == 0) return {0.f, 0.f};
     return {static_cast<float>(vector.x / magnitude), static_cast<float>(vector.y / magnitude)};
+}
+
+template <typename T, typename U>
+sf::Vector2f calculateDirection(T object, U point)
+{
+    sf::Vector2f direction = point.getPosition() - object.getPosition();
+    return normalize(direction);
 }
 
 template <typename T, typename U>
@@ -45,23 +50,14 @@ sf::Vector2f calculateForce(T &object, U &point)
     return dir * (float)forceMagnitude;
 }
 
-template <typename T, typename U>
-sf::Vector2f calculateDirection(T object, U point)
-{
-    sf::Vector2f direction = point.getPosition() - object.getPosition();
-    return normalize(direction);
-}
-
 template <typename T>
-void update(T &object, sf::Vector2f force, float delta_time)
+void update(T &object, float delta_time)
 {
-    object.setAcceleration(force / static_cast<float>(object.getMass()));
+    object.setAcceleration(object.getForce() / static_cast<float>(object.getMass()));
     object.setVelocity(object.getVelocity() + object.getAcceleration() * delta_time);
     object.setPosition(object.getPosition() + object.getVelocity() * delta_time);
-
+    object.resetForce(); // Reset force accumulator after each update
 }
-
-
 
 class body
 {
@@ -71,13 +67,14 @@ class body
     sf::Vector2f acceleration;
     sf::Vector2f velocity;
     sf::Vector2f position;
+    sf::Vector2f force; // Acumulador de fuerza
     sf::CircleShape shape;
 
     public:
 
     //Constructor for body Class
     body(double bodyMass, sf::Vector2f bodyPosition, sf::Vector2f initialVelocity = {0.0f, 50.0f})
-    : mass(bodyMass), position(bodyPosition), acceleration({0.0f, 0.0f}), velocity(initialVelocity)
+    : mass(bodyMass), position(bodyPosition), acceleration({0.0f, 0.0f}), velocity(initialVelocity), force({0.0f, 0.0f})
     {
         shape.setFillColor(sf::Color::White);
         float ratio = mass / ATTRACTOR_MASS; // Calculate the ratio of the body's mass to the attractor's mass
@@ -86,6 +83,8 @@ class body
         shape.setOrigin({radius,radius}); // Set the origin to the center of the circle
         shape.setPosition(position);
     }
+
+    std::deque<sf::Vector2f> trail;
 
     //GETTERS
 
@@ -109,12 +108,27 @@ class body
         return position;
     }
 
+    sf::Vector2f getForce() const
+    {
+        return force;
+    }
+
     sf::CircleShape getShape() const
     {
         return shape;
     }
 
     //setters
+    void addForce(sf::Vector2f f)
+    {
+        force += f;
+    }
+
+    void resetForce()
+    {
+        force = {0.0f, 0.0f};
+    }
+
     void setAcceleration(sf::Vector2f newAcceleration)
     {
         acceleration = newAcceleration;
@@ -139,19 +153,24 @@ class attractor
     private:
 
     double mass = ATTRACTOR_MASS;
-    sf::Vector2f position = {0.0f, 0.0f};
+    sf::Vector2f acceleration;
+    sf::Vector2f velocity;
+    sf::Vector2f position;
+    sf::Vector2f force; // Acumulador de fuerza
     sf::CircleShape shape;
 
     public:
 
     // Constructor for the attractor class
-    attractor(sf::Vector2f pos) : position(pos)
+    attractor(sf::Vector2f pos) : position(pos), acceleration({0.0f, 0.0f}), velocity({0.0f, 0.0f}), force({0.0f, 0.0f})
     {   
         shape.setFillColor(sf::Color::Red);
         shape.setRadius(ATTRACTOR_RADIUS);
         shape.setOrigin({ATTRACTOR_RADIUS, ATTRACTOR_RADIUS}); // Set the origin to the center of the circle
         shape.setPosition(position);
     }
+
+    std::deque<sf::Vector2f> trail;
 
     //GETTERS
 
@@ -170,6 +189,49 @@ class attractor
         return shape;
     }
 
+    sf::Vector2f getAcceleration() const
+    {
+        return acceleration;
+    }
+
+    sf::Vector2f getVelocity() const
+    {
+        return velocity;
+    }
+
+    sf::Vector2f getForce() const
+    {
+        return force;
+    }
+
+    //SETTERS
+
+    void addForce(sf::Vector2f f)
+    {
+        force += f;
+    }
+
+    void resetForce()
+    {
+        force = {0.0f, 0.0f};
+    }
+
+    void setPosition(sf::Vector2f newPosition)
+    {
+        position = newPosition;
+        shape.setPosition(position);
+    }
+
+    void setAcceleration(sf::Vector2f newAcceleration)
+    {
+        acceleration = newAcceleration;
+    }
+
+    void setVelocity(sf::Vector2f newVelocity)
+    {
+        velocity = newVelocity;
+    }
+
 };
 
 int main()
@@ -180,10 +242,12 @@ int main()
 
     //Create random number generator for body mass
     //Used logarithmic distribution so big planets are more unlikely to appear
-    //logDis will generate numbers between 10^-4 and 10^1, thats why [-6.0, -3.0] is used as parameters, then we will power 10 to the generated number to get the mass of the body
+    //logDis will generate numbers between 10^-4 and 10^1, thats why [-2.0, 1.0] is used as parameters, then we will power 10 to the generated number to get the mass of the body
     std::random_device rd;
     std::mt19937 gen(1);
     std::uniform_real_distribution<float> logDis(-2.0f, 1.0f);
+
+    const int maxTrail = 2000;
 
 
     //Initialize window
@@ -222,31 +286,60 @@ int main()
         
         window.clear(sf::Color(0X0B213C));
 
-        for(int j = 0; j < bodies.size(); j++)
+        
+        for(int i = 0; i < bodies.size(); i++)
         {
-            sf::Vector2f totalForce = {0.0f, 0.0f};
-            for(int i = 0; i < attractors.size(); i++)
+            for(int j = 0; j < attractors.size(); j++)
             {
-                sf::Vector2f force = calculateForce(bodies[j], attractors[i]);
-                totalForce += force;
+                sf::Vector2f f = calculateForce(bodies[i], attractors[j]);
+                bodies[i].addForce(f);
+                attractors[j].addForce(-f); // Newton's third law
             }
-            update(bodies[j], totalForce, delta_time);
-            std::cout << "Body " << j << " acceleration: " << bodies[j].getAcceleration().x << ", " << bodies[j].getAcceleration().y << std::endl;
+        }
+
+        //Updating state of bodies and attractors
+        for(int i = 0; i < bodies.size(); i++)
+        {
+            update(bodies[i], delta_time);
+            bodies[i].trail.push_front(bodies[i].getPosition());
+            if (bodies[i].trail.size() > maxTrail){
+                bodies[i].trail.pop_back();
+            }
         }
 
         for(int i = 0; i < attractors.size(); i++)
         {
-            for(int j = 0; j < bodies.size(); j++)
-            {
-                std::array line ={
-                sf::Vertex{sf::Vector2f(bodies[j].getPosition())},
-                sf::Vertex{sf::Vector2f(attractors[i].getPosition())}
-                };
-
-                window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
+            update(attractors[i], delta_time);
+            attractors[i].trail.push_front(attractors[i].getPosition());
+            if (attractors[i].trail.size() > maxTrail){
+                attractors[i].trail.pop_back();
             }
         }
 
+        //Drawing trails
+        for (const auto& obj : bodies)
+        {
+            for (const auto& pos : obj.trail)
+            {
+                sf::CircleShape point(0.8f);
+                point.setFillColor(sf::Color::Yellow);
+                point.setPosition(pos);
+                window.draw(point);
+            }
+        }
+
+        for (const auto& obj : attractors)
+        {
+            for (const auto& pos : obj.trail)
+            {
+                sf::CircleShape point(0.8f);
+                point.setFillColor(sf::Color::Yellow);
+                point.setPosition(pos);
+                window.draw(point);
+            }
+        }
+
+        //Drawing bodies and attractors
         for(int i = 0; i < attractors.size(); i++)
         {
             window.draw(attractors[i].getShape());
@@ -261,236 +354,3 @@ int main()
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*OG CODE
-
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include <iostream>
-#include <cmath>
-#include <random>
-
-const double G = 5000.0f; // Gravitational constant (arbitrary units)
-const float SOFTENING = 200.0f; // Softening factor to prevent singularity at zero distance
-
-
-template <typename T, typename U>
-
-//Template to facilitate calculation between any given two points regardless of type, as long as they have getPosition() and getMass() methods.
-//Template to calculate the direction from a given object to a point, returns a normalized vector.
-sf::Vector2f calculateDirection(T object, U point)
-{
-    sf::Vector2f direction = point.getPosition() - object.getPosition();
-    float magnitude = std::sqrt(std::pow(direction.x, 2) + std::pow(direction.y, 2));
-    
-    if (magnitude == 0) return {0.f, 0.f};
-    return direction / magnitude;
-}
-
-
-template <typename T, typename U>
-sf::Vector2f calculateForce(T object, U point)
-{
-    double distance = sqrt(pow(point.getPosition().x - object.getPosition().x, 2) + pow(point.getPosition().y - object.getPosition().y, 2));
-    sf::Vector2f force_dir = calculateDirection(object, point);
-    float force_magnitude = (G * point.getMass() * object.getMass()) / (distance * distance + SOFTENING);
-    std::cout << "force magnitude: " << force_magnitude << " distance: " << distance  << "acceleration: " << force_magnitude / object.getMass() << "mass: " << object.getMass() << std::endl;
-    if(distance < 0.1f) return {0.f, 0.f};
-    return sf::Vector2f(force_dir.x * force_magnitude, force_dir.y * force_magnitude);
-}
-
-//Body class to represent any moving object, planets, meteors, etc.
-class body
-{
-    private:
-        float mass;
-        sf::Vector2f position;
-        sf::Vector2f velocity;
-        sf::Vector2f acceleration;
-        sf::CircleShape shape;
-    
-    public: 
-
-    //default constructor
-    body(float m = 50, sf::Vector2f pos = {50.0f,50.0f}, sf::Vector2f initial_velocity = {10.0f,10.0f}, sf::Vector2f acc = {0.0f,0.0f}) : mass(m), position(pos), velocity(initial_velocity), acceleration(acc)
-    {
-        float radius = std::sqrt(mass) / 2.0f; // Scale down the radius for better visualization
-        shape.setRadius(radius);
-        shape.setPosition(position);
-        shape.setOrigin(sf::Vector2f(radius, radius));
-        shape.setFillColor(sf::Color::White);
-    }
-
-    sf::Vector2f getVelocity()
-    {
-        return velocity;
-    }
-
-    sf::Vector2f getAcceleration()
-    {
-        return acceleration;
-    }
-
-    sf::Vector2f getPosition()
-    {
-        return position;
-    }
-
-    float getMass()
-    {
-        return mass;
-    }
-
-    //method used to update the position and velocity of the body based on the total force applied and the time elapsed since the last update.
-    void update(sf::Vector2f force, float delta_time)
-    {
-        acceleration = force / mass;
-        velocity += acceleration * delta_time;
-        position += velocity * delta_time;
-        shape.setPosition(position);
-    }
-
-    void draw(sf::RenderWindow& window) 
-    {
-    window.draw(shape);
-    }
-};
-
-//Class to represent places of gravity, such as stars. Represented by a red circle of a given mass.
-
-class gravity_point
-{
-    private:
-    sf::Vector2f position;
-    float mass;
-    sf::CircleShape shape;
-
-    public: 
-    gravity_point(float m = 10.0f, sf::Vector2f pos = {500.0f, 500.0f}) : position(pos), mass(m)
-    {
-        float radius = std::sqrt(mass) / 2.0f; // Scale down the radius for better visualization
-        shape.setRadius(radius);
-        shape.setPosition(position);
-        shape.setOrigin(sf::Vector2f(radius, radius));
-        shape.setFillColor(sf::Color::Red);
-    }
-
-    sf::Vector2f getPosition()
-    {
-        return position;
-    }
-
-    float getMass()
-    {
-        return mass;
-    }
-
-    void draw(sf::RenderWindow& window) 
-    {
-    window.draw(shape);
-    }
-};
-
-
-
-int main()
-{
-    //Initialization of the window, clock, vectors, and random number generator for the mass of the bodies.
-    //Resolution affects simulation 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Gravity Simulation");
-    sf::Clock clock;
-    std::vector<body> bodies;
-    std::vector<gravity_point> gravity_points;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(50.0f, 200.0f);
-
-    while (window.isOpen()) {
-        
-        //Calculate the time elapsed since the last frame, which is used to ensure that the simulation runs at a consistent speed regardless of the frame rate.
-        float delta_time = clock.restart().asSeconds();
-
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-            //Check for mouse button press
-            //if left click is press, a gravity point appears
-            //if right click is press, a body appears with a random mass and an initial velocity of 150 in the y direction.
-            if (const auto* mouseClick = event->getIf<sf::Event::MouseButtonPressed>()) {
-
-                if(mouseClick->button == sf::Mouse::Button::Left)
-                {   
-                    gravity_points.push_back(gravity_point(1000.0f, sf::Vector2f(mouseClick->position)));
-                }
-
-                if(mouseClick->button == sf::Mouse::Button::Right)
-                {
-                    sf::Vector2i click_pos = mouseClick->position;
-                    bodies.push_back(body(dis(gen), sf::Vector2f(click_pos), {0.0f, 150.0f}, {0.0f, 0.0f}));
-                }
-
-            }
-        }
-
-
-        for(int i = 0; i < bodies.size(); i++)
-        {
-            sf::Vector2f total_force = {0.0f, 0.0f};
-            for(int j = 0; j < gravity_points.size(); j++)
-            {
-                total_force += calculateForce(bodies[i], gravity_points[j]);
-            }
-
-            for (int k = i + 1; k < bodies.size(); k++) 
-            {
-            sf::Vector2f f = calculateForce(bodies[i], bodies[k]);
-            bodies[i] += f;
-            bodies[k] -= f; // Tercera Ley de Newton: Ahorras el 50% del cálculo
-            }
-
-            bodies[i].update(total_force, delta_time);
-        }
-        
-        
-        window.clear();
-        for(body& first_body : bodies)
-        {
-            first_body.draw(window);
-        }
-        for(auto& first_gravity : gravity_points)
-        {
-            first_gravity.draw(window);
-        }
-        window.display();
-
-        
-    }
-
-    
-
-
-
-}
-
-*/
